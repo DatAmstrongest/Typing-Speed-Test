f2a8ab46ced6a8cc5cfcb72da8898758c250aa2d
kocmetehan <metehankoc51@gmail.com>
2024-07-29 19:49:32 +0300

    Restart button is added to the results screen

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from tkinter import *
 import requests
 
+TIME_LABEL = None
+TEXT = None
+TEXT_INPUT = None
+
 WORDS = []
 CURRENT_WORDS = []
 CURRENT_GROUP_INDEX = 0
@@ -21,9 +25,11 @@ TIME_UPPER_LIMIT = 5
 CURRENT_TIME = TIME_UPPER_LIMIT
 
 def callback(var_x):
+    global TEXT_INPUT
+
     if len(var_x.get()) and (var_x.get()[-1] == " " or var_x.get()[-1] == ""):
         word = var_x.get()
-        text_input.delete(0, END)
+        TEXT_INPUT.delete(0, END)
         color_word(word)
         if CURRENT_WORD_INDEX == SHOWN_WORD_UPPER_LIMIT:
             fetch_new_words()
@@ -43,17 +49,19 @@ def color_word(word):
 
 def color_correct_word():
     global CORRECT_WORDS_NUM
+    global TEXT
 
     CORRECT_WORDS_NUM += 1
-    text.tag_add("first_word", f"1.{CURRENT_INDEX}", f"1.{CURRENT_INDEX+len(CURRENT_WORDS[CURRENT_WORD_INDEX])}")
-    text.tag_config("first_word", foreground=CORRECT_COLOR)
+    TEXT.tag_add("first_word", f"1.{CURRENT_INDEX}", f"1.{CURRENT_INDEX+len(CURRENT_WORDS[CURRENT_WORD_INDEX])}")
+    TEXT.tag_config("first_word", foreground=CORRECT_COLOR)
 
 def color_wrong_word():
     global WRONG_WORDS_NUM
+    global TEXT
 
     WRONG_WORDS_NUM += 1
-    text.tag_add("second_word", f"1.{CURRENT_INDEX}", f"1.{CURRENT_INDEX+len(CURRENT_WORDS[CURRENT_WORD_INDEX])}")
-    text.tag_config("second_word", foreground=FALSE_COLOR)
+    TEXT.tag_add("second_word", f"1.{CURRENT_INDEX}", f"1.{CURRENT_INDEX+len(CURRENT_WORDS[CURRENT_WORD_INDEX])}")
+    TEXT.tag_config("second_word", foreground=FALSE_COLOR)
 
 
 def fetch_new_words():
@@ -61,6 +69,7 @@ def fetch_new_words():
     global CURRENT_GROUP_INDEX
     global CURRENT_INDEX
     global CURRENT_WORDS
+    global TEXT
 
     CURRENT_GROUP_INDEX += 1
     CURRENT_WORD_INDEX = 0  
@@ -72,11 +81,12 @@ def fetch_new_words():
     else:
         CURRENT_WORDS = WORDS[SHOWN_WORD_UPPER_LIMIT*CURRENT_GROUP_INDEX: SHOWN_WORD_UPPER_LIMIT*CURRENT_GROUP_INDEX+SHOWN_WORD_UPPER_LIMIT]
     CURRENT_INDEX = 0
-    text.config(state=NORMAL)
-    text.delete("1.0", END)
-    text.insert(INSERT, " ".join(CURRENT_WORDS))
-    text.config(state=DISABLED)
-    text.tag_configure("center", justify='center')
+    TEXT.config(state=NORMAL)
+    TEXT.delete("1.0", END)
+    TEXT.insert(INSERT, " ".join(CURRENT_WORDS))
+    TEXT.config(state=DISABLED)
+    TEXT.tag_configure("center", justify='center')
+
 
 def show_game_result():
     for child in root.winfo_children(): 
@@ -109,25 +119,79 @@ def show_game_result():
     net_speed_value_label.place(x=550, y=50)
     net_speed_label = Label(root, text="Net Speed", font=("Open Sans", 16), fg=BACKGROUND_COLOR, bg=FOREGROUND_COLOR)
     net_speed_label.place(x=540, y=110)
-      
+    
+    restart_game_button = Button(root, text="Restart Game", command=restart_game)
+    restart_game_button.pack()
+
+def restart_game():
+    for child in root.winfo_children(): 
+        child.destroy()
+    setup_game()
+
+def setup_game():
+    global CURRENT_WORDS
+    global CURRENT_GROUP_INDEX
+    global CURRENT_WORD_INDEX
+    global CURRENT_INDEX
+
+    global CORRECT_WORDS_NUM
+    global WRONG_WORDS_NUM
+    global TOTAL_WRITTEN_WORDS
+
+    global CURRENT_TIME
+    global TIME_LABEL
+
+    global TEXT
+    global TEXT_INPUT
+
+    CURRENT_WORDS = []
+    CURRENT_GROUP_INDEX = 0
+    CURRENT_WORD_INDEX = 0
+    CURRENT_INDEX = 0
+
+    CORRECT_WORDS_NUM = 0
+    WRONG_WORDS_NUM = 0
+    TOTAL_WRITTEN_WORDS = 0
+    CURRENT_TIME = TIME_UPPER_LIMIT
+
+    var_x = StringVar()
+    var_x.trace("w", lambda name, index, mode, var_x=var_x: callback(var_x))
+    TEXT_INPUT = Entry(root, background=FOREGROUND_COLOR, foreground=BACKGROUND_COLOR, textvariable=var_x, font=("Open Sans", 16))
+
+    read_file()
+    TEXT = Text(root, highlightthickness = 0, borderwidth=0, width=75, height=5, fg=FOREGROUND_COLOR, background=BACKGROUND_COLOR, font=("Open Sans", 16))
+    TEXT.tag_configure("center", justify='center')
+    TEXT.insert(INSERT, " ".join(CURRENT_WORDS))
+
+
+    TIME_LABEL = Label(root, text = CURRENT_TIME, bg=BACKGROUND_COLOR, fg=FOREGROUND_COLOR, font=("Open Sans", 16))
+    TIME_LABEL.pack(side=TOP, anchor=NW, pady=(20,0), padx=(30,0))
+    timer = root.after(1000, update_timer)
+    # adding a tag to a part of text specifying the indices
+    TEXT.tag_add("center", "1.0", "end") 
+    TEXT.configure(state=DISABLED)
+    TEXT.pack(pady=(20,0))
+    TEXT_INPUT.pack(expand=True, fill='both', padx=(60, 60), pady=(0,100))
+
+
 def read_file():
     global WORDS
     global CURRENT_WORDS
-    url = requests.get("https://en.wikipedia.org/wiki/Special:Random/")
-    print(url.text)
     with open("./assets/sample_text.txt", "r") as file:
         lines = file.readlines()
         for line in lines:
             WORDS = WORDS + [word.strip() for word in line.strip().split(" ") if word.strip()]
         CURRENT_WORDS =  WORDS[0: SHOWN_WORD_UPPER_LIMIT]
 
-def update_timer():  
+def update_timer():
     global CURRENT_TIME
+    global TIME_LABEL
+
     CURRENT_TIME -= 1
     if CURRENT_TIME < 0:
         show_game_result()
     else:
-        time_label.configure(text=CURRENT_TIME)
+        TIME_LABEL.configure(text=CURRENT_TIME)
         root.after(1000, update_timer)
 
 
@@ -135,23 +199,7 @@ root = Tk()
 root.config(bg=BACKGROUND_COLOR)
 root.geometry("800x300")
 
-var_x = StringVar()
-var_x.trace("w", lambda name, index, mode, var_x=var_x: callback(var_x))
-text_input = Entry(root, background=FOREGROUND_COLOR, foreground=BACKGROUND_COLOR, textvariable=var_x, font=("Open Sans", 16))
-
-read_file()
-text = Text(root, highlightthickness = 0, borderwidth=0, width=75, height=5, fg=FOREGROUND_COLOR, background=BACKGROUND_COLOR, font=("Open Sans", 16))
-text.tag_configure("center", justify='center')
-text.insert(INSERT, " ".join(CURRENT_WORDS))
-
-time_label = Label(root, text = CURRENT_TIME, bg=BACKGROUND_COLOR, fg=FOREGROUND_COLOR, font=("Open Sans", 16))
-time_label.pack(side=TOP, anchor=NW, pady=(20,0), padx=(30,0))
-timer = root.after(1000, update_timer)
-# adding a tag to a part of text specifying the indices
-text.tag_add("center", "1.0", "end") 
-text.configure(state=DISABLED)
-text.pack(pady=(20,0))
-text_input.pack(expand=True, fill='both', padx=(60, 60), pady=(0,100))
+setup_game()
 
 
 root.mainloop()
